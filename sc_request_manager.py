250487ea88e33a214acecb6747b08aa956552a4c
TR <teddy@teddyrowan.com>
2020-09-26 03:05:53 -0400

    Improved error catching and documentation in sc_request_manager.py

--- sc_request_manager.py
+++ sc_request_manager.py
@@ -5,7 +5,7 @@ Request handling for benchmark correlation comparison tool.
 
 sc_request_manager.py
 Author: Teddy Rowan @ MySybil.com
-Last Modified: August 10, 2020
+Last Modified: Sept 26, 2020
 Description: this script handles the requests for correlation scripts.
 
 """
@@ -15,19 +15,40 @@ import sc_settings
 
 root_url = 'https://sandbox.tradier.com/v1/markets'
 
-# Download the historic trading data for the symbol
 def get_history(symbol, interval, start_date):
-    response = requests.get(root_url + '/history',
-        params={'symbol': symbol, 
-                'interval': interval, 
-                'start': start_date},
-        headers={'Authorization': sc_settings.api_key(), 
-                 'Accept': 'application/json'}
-    )
-    json_response = response.json()
+    """ 
+    GET request to the Tradier API to download daily stock data
+    for the requested symbol (either input or benchmark). Verify
+    the data is valid and then return the json response.
+    
+    Parameters
+    ----------
+    symbol : 
+        Stock ticker for the 
+    interval : 
+        'daily', 'weekly', or 'monthly'
+    start_date : 
+        A date string in %Y-%m-d format (eg. 2019-01-01)
+
+
+    Returned Variables [1]
+    ----------------------
+    <list> : 
+        A list of dicts for each trading interval. Lists have keys:
+        'date', 'open', 'high', 'low', 'close'
+        
+    """
 
     try:
-        json_data = json_response['history']['day']
+        response = requests.get(root_url + '/history',
+            params={'symbol': symbol, 
+                    'interval': interval, 
+                    'start': start_date},
+            headers={'Authorization': sc_settings.api_key(), 
+                     'Accept': 'application/json'}
+        ).json()
+    
+        json_data = response['history']['day']
         return json_data
     except TypeError:
         return -1
